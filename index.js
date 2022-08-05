567e85136b56bc7165d9bf7eac62174bd61447fc
AlanPedroD <alandiaslp@hotmail.com>
2022-08-04 21:54:28 -0300

    condicionais js

---
+++ index.js
@@ -0,0 +1,36 @@
+function pulaLinha() {
+    document.write("<br>");
+}
+
+function mostra(frase) {
+    document.write("<p>" + frase + "</p>");
+}
+
+function calculaImc(altura, peso) {
+
+    return peso / ( altura * altura );
+}
+
+var nome = prompt ("Qual é seu nome?");
+var alturaInformada = (prompt("digite a sua altura:"));
+var pesoInformado =  (prompt("digite o seu peso:"));
+var imc = calculaImc(alturaInformada, pesoInformado);
+
+if (imc < 18.5) {
+    mostra("Olá " + nome + " o seu imc é " + imc.toFixed(2));
+    pulaLinha();
+    mostra ("Seu imc está abaixo da média")
+}
+
+if (imc > 35) {
+    mostra("Olá " + nome + " o seu imc é " + imc.toFixed(2));
+    pulaLinha();
+    mostra ("Seu imc está acima do recomendado")
+}
+
+if (imc >= 18.5 && imc <= 35)  {
+    mostra("Olá " + "<strong>" + nome + "</strong>" + " o seu imc é " + "<strong>" +imc.toFixed(2) + "</strong>");
+    pulaLinha();
+    mostra ("Você está dentro da média recomendada")
+    mostra ("Parabens !!!");
+}
